fac09c90aea38d3f7f0e1c5100e7a3b14053ed91
Nobby-code <norbertguda0@gmail.com>
2022-10-15 12:51:38 -0700

    Creating a file in /tmp using Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# creating a file in emp directory
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
